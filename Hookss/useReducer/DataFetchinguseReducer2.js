fcc56560d3c18f20e99046e092b77d8394da049c
Devanshi Gupta <devanshigupta2002@gmail.com>
2023-03-16 01:49:42 +0530

    files added

---
+++ Hookss/useReducer/DataFetchinguseReducer2.js
@@ -0,0 +1,53 @@
+import React,{useState,useEffect, useReducer} from "react";
+import axios from 'axios' 
+
+const initialState={
+    loading:true,
+    post:{}, 
+    error:''
+}
+
+const reducer=(state,action)=>{
+    switch(action.type){
+        case 'FETCH_SUCCESS':
+            return{
+                loading:false,
+                post:action.dataload,
+                error:''
+            }
+        case 'FETCH_ERROR':
+            return{
+                loading:false,
+                post:'',
+                error:'Something went wrong'
+            }
+        default:
+            return state
+
+    }
+}
+
+
+function DataFetchingTwo(){
+
+    const [state,dispatch]=useReducer(reducer,initialState)
+
+    useEffect(()=>{
+        axios.get("https://jsonplaceholder.typicode.com/posts/1")
+        .then(response=>{
+            dispatch({type:'FETCH_SUCCESS', dataload:response.data})
+        })
+        .catch(error=>{
+            dispatch({type : 'FETCH_ERROR'})
+        })
+    },[])
+
+    return(
+        <div>
+            {state.loading ? 'Loading' : state.post.title}
+            {state.error ? state.error : null}
+        </div>
+    )
+}
+
+export default DataFetchingTwo
